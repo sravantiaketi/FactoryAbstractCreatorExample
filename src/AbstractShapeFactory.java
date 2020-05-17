eb8b9c1fa5e1514b5b7e013103d8ae06fd1b178e
Sravanti Aketi <sravanti.aketi@gmail.com>
2020-05-17 17:16:36 +1000

    first commit

---
+++ src/AbstractShapeFactory.java
@@ -0,0 +1,33 @@
+public abstract class AbstractShapeFactory {
+
+    protected abstract Shape factoryMethod();
+
+    public Shape getShape() {
+        return factoryMethod();
+    }
+}
+
+
+class RectangleFactory extends  AbstractShapeFactory {
+
+    protected Shape factoryMethod() {
+        return  new Rectangle();
+    }
+
+}
+
+class SquareFactory extends  AbstractShapeFactory {
+
+    protected Shape factoryMethod() {
+        return  new Square();
+    }
+
+}
+
+class CircleFactory extends  AbstractShapeFactory {
+
+    protected Shape factoryMethod() {
+        return  new Circle();
+    }
+
+}
---
+++ src/Client.java
@@ -0,0 +1,13 @@
+public class Client {
+
+    public static void main(String[] args) {
+        Shape shape1 = new CircleFactory().getShape();
+        shape1.draw();
+
+        Shape shape2 = new SquareFactory().getShape();
+        shape2.draw();
+
+        Shape shape3 = new RectangleFactory().getShape();
+        shape3.draw();
+    }
+}
